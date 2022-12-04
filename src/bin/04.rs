815787a555200989ac4b86d80f86305568a85ed8
Greg Woods <gregwoods01@gmail.com>
2022-12-04 01:21:37 -0500

    Day 4

---
+++ src/bin/04.rs
@@ -0,0 +1,63 @@
+use std::ops::RangeInclusive;
+
+use advent_of_code::helpers::vec_of_strings;
+use regex::Regex;
+
+pub fn part_one(input: &str) -> Option<u32> {
+    let lines = vec_of_strings(input);
+    let regex = Regex::new(r"[,-]").unwrap();
+    let mut count = 0;
+
+    for line in lines {
+        let numbers: Vec<u32> = regex.split(line).map(|str| str.parse().unwrap()).collect();
+
+        if numbers[0] <= numbers[2] && numbers[1] >= numbers[3] || numbers[0] >= numbers[2] && numbers[1] <= numbers[3] {
+            count += 1;
+        }
+    }
+
+    Some(count)
+}
+
+fn range_intersects(a: RangeInclusive<u32>, b: RangeInclusive<u32>) -> bool {
+    a.contains(b.start()) || a.contains(b.end()) || b.contains(a.start()) || b.contains(a.end())
+}
+
+pub fn part_two(input: &str) -> Option<u32> {
+    let lines = vec_of_strings(input);
+    let regex = Regex::new(r"[,-]").unwrap();
+    let mut count = 0;
+
+    for line in lines {
+        let numbers: Vec<u32> = regex.split(line).map(|str| str.parse().unwrap()).collect();
+
+        if range_intersects(numbers[0]..=numbers[1], numbers[2]..=numbers[3]) {
+            count += 1;
+        }
+    }
+
+    Some(count)
+}
+
+fn main() {
+    let input = &advent_of_code::read_file("inputs", 4);
+    advent_of_code::solve!(1, part_one, input);
+    advent_of_code::solve!(2, part_two, input);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_one() {
+        let input = advent_of_code::read_file("examples", 4);
+        assert_eq!(part_one(&input), Some(2));
+    }
+
+    #[test]
+    fn test_part_two() {
+        let input = advent_of_code::read_file("examples", 4);
+        assert_eq!(part_two(&input), Some(4));
+    }
+}
